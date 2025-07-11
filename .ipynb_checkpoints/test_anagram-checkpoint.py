842fb605249a51cadac133e7b6c54c9fbac6bdd3
tobubu-ops <66975515+tobubu-ops@users.noreply.github.com>
2025-07-11 13:33:37 +0100

    Initial commit with anagram notebook and test files

---
+++ .ipynb_checkpoints/test_anagram-checkpoint.py
@@ -0,0 +1,47 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "7eedd6a7",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "# test_anagram.py\n",
+    "\n",
+    "def test_basic_anagram():\n",
+    "    from anagram import is_anagram\n",
+    "    assert is_anagram(\"listen\", \"silent\") == True"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "5fc1dc25",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.8.8"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
